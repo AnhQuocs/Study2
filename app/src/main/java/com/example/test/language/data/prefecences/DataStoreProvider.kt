54674bd4090df84aa15ccd5742545137a4b2020c
AnhQuocs <anhquocydkl@gmail.com>
2025-10-29 19:18:39 +0700

    refactor: remove language feature

--- app/src/main/java/com/example/test/language/data/prefecences/DataStoreProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.example.test.language.data.prefecences
-
-import android.content.Context
-import androidx.datastore.preferences.preferencesDataStore
-
-val Context.languageDataStore by preferencesDataStore(name = "settings")
--- app/src/main/java/com/example/test/language/domain/usecase/LanguageUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test.language.domain.usecase
-
-import javax.inject.Inject
-
-data class LanguageUseCases @Inject constructor(
-    val getLanguage: GetLanguageUseCase,
-    val updateLanguage: UpdateLanguageUseCase
-)
